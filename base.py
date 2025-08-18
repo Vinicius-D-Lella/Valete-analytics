afd73be31a8c40fc69f4405c376cae03d94fcedf
Vinicius-D-Lella <viniciusdilella46@gmail.com>
2025-08-18 15:49:40 -0300

    botão login/logout

--- base.py
+++ base.py
@@ -5,6 +5,7 @@ if not st.user.is_logged_in:
     st.write("Por favor, faça login para continuar, caso não tenha acesso fale com o administrador.")
     if st.button("Log in com Google"):
         st.login()
+
 pages = {
     "Menu": [
         st.Page("home.py", title="Resumo do Dia"),
@@ -14,7 +15,6 @@ pages = {
         #st.Page("bar_chart.py", title="Comparação entre views de graça ou pagas"),
     ]
 }
-
 if st.user.is_logged_in:
     if st.user.email in st.secrets["whitelist"]:
         pg = st.navigation(pages)
@@ -22,4 +22,6 @@ if st.user.is_logged_in:
         pg.run()
     else:
         st.write("Você não tem permissão para acessar esta página.")
+        if st.button("Log out"):
+            st.logout()
 
