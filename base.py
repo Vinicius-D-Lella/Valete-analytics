4758a136d0f8bb57ea8a2acd47a92e0b7f306cc5
Vinicius-D-Lella <viniciusdilella46@gmail.com>
2025-08-18 15:44:59 -0300

    botão login/logout

--- base.py
+++ base.py
@@ -1,18 +1,12 @@
 import streamlit as st
 
 if not st.user.is_logged_in:
-    if st.button("Log in"):
+    st.title("Login")
+    st.write("Por favor, faça login para continuar, caso não tenha acesso fale com o administrador.")
+    if st.button("Log in com Google"):
         st.login()
-else:
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.button("Log out"):
-            st.logout()
-    with col2:
-        st.write("")
-
 pages = {
-    "Pages": [
+    "Menu": [
         st.Page("home.py", title="Resumo do Dia"),
         st.Page("module_views.py", title="Análise de Módulo"),
         st.Page("bar_chart_solo.py", title="Análise de Público"),
@@ -20,6 +14,7 @@ pages = {
         #st.Page("bar_chart.py", title="Comparação entre views de graça ou pagas"),
     ]
 }
+st.sidebar.button("Log out", on_click=st.logout)
 
 if st.user.is_logged_in:
     if st.user.email in st.secrets["whitelist"]:
